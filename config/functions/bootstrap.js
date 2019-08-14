96424eb01c0949624677e5dbb1506a238d14a684
Camden Phalen <camdenphalen@gmail.com>
2019-08-14 16:33:01 -0400

    even more setup

--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -1,12 +1,5 @@
-'use strict';
-
-/**
- * An asynchronous bootstrap function that runs before
- * your application gets started.
- *
- * This gives you an opportunity to set up your data model,
- * run jobs, or perform some special logic.
- */
+"use strict";
+require("dotenv").config({ path: require("find-config")(".env") });
 
 module.exports = cb => {
   cb();
